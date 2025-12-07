9c17fba84033680f7fd5471b44d9b9758d8e1dd8
preciousgit <preciousonyechere3@gmail.com>
2025-12-07 08:59:31 +0000

    Updated Seed Data

--- seed.js
+++ seed.js
@@ -5,16 +5,16 @@ const MONGO_URI =
   'mongodb+srv://preciousonyechere3_db_user:BHWbWFEdAlcJnHhS@learningwebapp.axjgbei.mongodb.net/CourseApp?appName=LearningWebApp';
 
 const lessons = [
-  { title: 'Mathematics', slug: 'mathematics', location: 'Hendon', price: 100, space: 5, img: 'images/mths1.jpg', category: 'Math' },
-  { title: 'Science Lab', slug: 'science-lab', location: 'Colindale', price: 95, space: 5, img: 'images/sci-lab2.jpg', category: 'Science' },
-  { title: 'Music', slug: 'music', location: 'Brent Cross', price: 80, space: 5, img: 'images/music3.jpg', category: 'Music' },
-  { title: 'Programming', slug: 'programming', location: 'Online', price: 120, space: 5, img: 'images/pgm1.jpg', category: 'Coding' },
-  { title: 'Art & Design', slug: 'art-design', location: 'Golders Green', price: 70, space: 5, img: 'images/art-d.jpg', category: 'Arts' },
-  { title: 'Football', slug: 'football', location: 'Hendon', price: 50, space: 5, img: 'images/fball1.jpg', category: 'Sports' },
-  { title: 'English Literature', slug: 'english-lit', location: 'Colindale', price: 85, space: 5, img: 'images/e-lit1.jpg', category: 'English' },
+  { title: 'Mathematics', slug: 'mathematics', location: 'Hendon', price: 100, space: 15, img: 'images/mths1.jpg', category: 'Math' },
+  { title: 'Science Lab', slug: 'science-lab', location: 'Colindale', price: 95, space: 13, img: 'images/sci-lab2.jpg', category: 'Science' },
+  { title: 'Music', slug: 'music', location: 'Brent Cross', price: 80, space: 10, img: 'images/music3.jpg', category: 'Music' },
+  { title: 'Programming', slug: 'programming', location: 'Online', price: 120, space: 9, img: 'images/pgm1.jpg', category: 'Coding' },
+  { title: 'Art & Design', slug: 'art-design', location: 'Golders Green', price: 70, space: 11, img: 'images/art-d.jpg', category: 'Arts' },
+  { title: 'Football', slug: 'football', location: 'Hendon', price: 50, space: 13, img: 'images/fball1.jpg', category: 'Sports' },
+  { title: 'English Literature', slug: 'english-lit', location: 'Colindale', price: 85, space: 15, img: 'images/e-lit1.jpg', category: 'English' },
   { title: 'Advanced Maths', slug: 'advanced-maths', location: 'Brent Cross', price: 130, space: 5, img: 'images/adv-mths.jpg', category: 'Math' },
   { title: 'Drama Club', slug: 'drama-club', location: 'Golders Green', price: 60, space: 5, img: 'images/drama4.jpg', category: 'Arts' },
-  { title: 'Robotics and Tech', slug: 'robotics-tech', location: 'Online', price: 140, space: 5, img: 'images/rob-tec3.jpg', category: 'Coding' }
+  { title: 'Robotics and Tech', slug: 'robotics-tech', location: 'Online', price: 140, space: 9, img: 'images/rob-tec3.jpg', category: 'Coding' }
 ];
 
 const orders = [
